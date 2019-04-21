ac15628417c365b8ecce7b81fab3d6e861034e11
zhuhaipeng <dell@qq.com>
2019-04-21 11:13:55 +0800

    update aspect

--- proxy/src/main/java/com/test/aop/Main.java
+++ proxy/src/main/java/com/test/aop/Main.java
@@ -1,15 +1,27 @@
 package com.test.aop;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] s) {
-
+		annotationSpring();
 	}
 	
-	public static void xmlTest1(){
+	/**
+	 * 使用注解启动spring
+	 */
+	public static void annotationSpring(){
+		AnnotationConfigApplicationContext ctxt = new AnnotationConfigApplicationContext(AppConfig.class);
+		ctxt.getBean(AspectTest.class).test();;
+	}
+	
+	/**
+	 * xml配置spring
+	 */
+	public static void xmlSpring(){
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:aop-application-context.xml");
 		XmlTest test = ctx.getBean(XmlTest.class);
 		test.test();
