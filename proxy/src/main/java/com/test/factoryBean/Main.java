760045de0d1c33c851f3ca1386a820ec15d1ec3e
zhuhaipeng <342451958@qq.com>
2019-05-26 23:00:30 +0800

    add

--- proxy/src/main/java/com/test/factoryBean/Main.java
+++ proxy/src/main/java/com/test/factoryBean/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
         /**
          * 这里如果用getBean(class)方法会报异常!
+         * 因为spring把对象转换成BD的过程中保存的是beanName作为key,
+         * 而MyFactoryBean的getObject返回的是Tmp而不是MyFactoryBean.class
+         *
          */
 //        Temp myFactoryBean = (Temp) ac.getBean(MyFactoryBean.class);
 
---
+++ proxy/src/main/java/com/test/importBeanDefinitionRegistry/Main.java
@@ -0,0 +1,39 @@
+package com.test.importBeanDefinitionRegistry;
+
+import com.test.proxy.cglib.CglibProxy;
+import com.test.proxy.cglib.CglibTarget;
+import com.test.proxy.javaProxy.JavaProxy;
+import com.test.proxy.javaProxy.Target;
+import com.test.proxy.javaProxy.TargetImpl;
+
+public class Main {
+	
+	public static void main(String[] ss){
+
+
+		// java 动态代理
+		javaproxy();
+
+		// cglib动态代理
+		System.out.println("-------------------");
+
+		cglibProxy();
+
+	}
+
+	private static void javaproxy(){
+		JavaProxy javaProxy = new JavaProxy(new TargetImpl());
+		Target proxy = (Target)javaProxy.newInstance();
+		proxy.sayHello("java");
+
+	}
+
+	private static  void cglibProxy(){
+		CglibProxy test2 = new CglibProxy();
+		CglibTarget target = (CglibTarget)test2.newProxyInstance(CglibTarget.class);
+		target.doSth("cglib..");
+
+	}
+	
+
+}
