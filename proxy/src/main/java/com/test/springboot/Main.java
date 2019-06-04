dee409ec84a335db24accb955f53fc19b31f5dac
zhuhaipeng <dell@qq.com>
2019-06-04 17:23:11 +0800

    update

--- proxy/src/main/java/com/test/springboot/Main.java
+++ proxy/src/main/java/com/test/springboot/Main.java
@@ -2,18 +2,29 @@ package com.test.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.EventPublishingRunListener;
 import org.springframework.util.StopWatch;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-//        SpringApplication.run(Main.class,args);
+        SpringApplication.run(Main.class,args);
         stopWatchTest();
+
+
+        //SpringApplication.getSpringFactoriesInstances()
+        // EventPublishingRunListener
+        // SimpleApplicationEventMulticaster # multicastEvent()
+        // SimpleApplicationEventMulticaster # doInvokeListener()
+        // DelegatingApplicationListener # onApplicationEvent()
+        // LiquibaseServiceLocatorApplicationListener # onApplicationEvent()
+
+
+
     }
 
     public static void stopWatchTest(){
         StopWatch sw = new StopWatch();
-
         sw.start("起床");
         try {
             Thread.sleep(1000);
