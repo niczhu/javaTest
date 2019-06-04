150e92c04b51faf3cfe8a6136cf79edbaf41d37a
zhuhaipeng <dell@qq.com>
2019-06-04 15:29:37 +0800

    add springboot

---
+++ proxy/src/main/java/com/test/springboot/Main.java
@@ -0,0 +1,32 @@
+package com.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StopWatch;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+//        SpringApplication.run(Main.class,args);
+        stopWatchTest();
+    }
+
+    public static void stopWatchTest(){
+        StopWatch sw = new StopWatch();
+
+        sw.start("起床");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sw.stop();
+
+        System.out.println(sw.prettyPrint());
+
+        System.out.println(sw.getTotalTimeMillis());
+        System.out.println(sw.getLastTaskName());
+        System.out.println(sw.getLastTaskInfo());
+        System.out.println(sw.getTaskCount());
+    }
+}
