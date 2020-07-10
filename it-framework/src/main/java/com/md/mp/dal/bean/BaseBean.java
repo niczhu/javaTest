46d677582d3dccefa3aa711957c291319acdd089
zhuhaipeng6 <zhuhaipeng6@jd.com>
2020-07-10 18:12:40 +0800

    update

--- it-framework/src/main/java/com/md/mp/dal/bean/BaseBean.java
+++ it-framework/src/main/java/com/md/mp/dal/bean/BaseBean.java
@@ -1,12 +1,14 @@
 package com.md.mp.dal.bean;
 
+import java.io.Serializable;
+
 /**
  * BaseBean
  *
  * @author zhuhaipeng6 in 2020/5/11 15:39
  * @version 1.0
  **/
-public class BaseBean {
+public class BaseBean implements Serializable {
 
     private String id;
 
